3c1f1868777c2f999ea7941cb3f1cff4c8b3cbb5
Iqbal <hellofromiqbal@gmail.com>
2023-01-28 11:09:54 +0700

    fourth commit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,22 +6,20 @@ const Contact = () => {
     <section className="contact__container" id="contact">
       <h2>Contact <span>Me</span></h2>
       <div className="contact__content">
-        <div className="contact__content-left">
-          <form action="">
-            <input name='name' type="text" placeholder='Your Name' autoComplete='off' />
-            <input name='email' type="email" placeholder='Your Email' autoComplete='off' />
-            <textarea name="message" cols="30" placeholder='Your Message'></textarea>
-            <button className="btn btn-primary">SEND MESSAGE</button>
-          </form>
-        </div>
-        <div className="contact__content-right">
-          <article className="contact-card">
-            <AiOutlineMail className='contact-icon'/>
+        <form action="">
+          <input name='name' type="text" placeholder='Your Name' autoComplete='off' />
+          <input name='email' type="email" placeholder='Your Email' autoComplete='off' />
+          <textarea name="message" cols="30" placeholder='Your Message'></textarea>
+          <button className="btn btn-primary">SEND MESSAGE</button>
+        </form>
+        <div className="contact__alternative-list">
+          <article className="alternative-card">
+            <AiOutlineMail className='alternative-icon'/>
             <h3>Email</h3>
             <small>frankabagnale@email.com</small>
           </article>
-          <article className="contact-card">
-            <AiOutlinePhone className='contact-icon'/>
+          <article className="alternative-card">
+            <AiOutlinePhone className='alternative-icon'/>
             <h3>Phone</h3>
             <small>+1 234 5678 9098</small>
           </article>
