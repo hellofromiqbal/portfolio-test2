5578c13c69024aa83036a40f8ffb28e557235e9b
Iqbal <hellofromiqbal@gmail.com>
2023-01-28 11:19:01 +0700

    fifth commit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,20 +6,14 @@ const Contact = () => {
     <section className="contact__container" id="contact">
       <h2>Contact <span>Me</span></h2>
       <div className="contact__content">
-        <form action="">
-          <input name='name' type="text" placeholder='Your Name' autoComplete='off' />
-          <input name='email' type="email" placeholder='Your Email' autoComplete='off' />
-          <textarea name="message" cols="30" placeholder='Your Message'></textarea>
-          <button className="btn btn-primary">SEND MESSAGE</button>
-        </form>
-        <div className="contact__alternative-list">
-          <article className="alternative-card">
-            <AiOutlineMail className='alternative-icon'/>
+        <div className="contact__card-list">
+          <article className="card-item">
+            <AiOutlineMail className='card-icon'/>
             <h3>Email</h3>
             <small>frankabagnale@email.com</small>
           </article>
-          <article className="alternative-card">
-            <AiOutlinePhone className='alternative-icon'/>
+          <article className="card-item">
+            <AiOutlinePhone className='card-icon'/>
             <h3>Phone</h3>
             <small>+1 234 5678 9098</small>
           </article>
